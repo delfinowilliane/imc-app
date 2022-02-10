b4435e24bf66672346d7a1f1898895223dd40b99
Williane Delfino <willy.pinheiro.94@gmail.com>
2022-02-10 16:21:44 -0300

    My first commit
    imc-app

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import { useState } from 'react'
+import './App.css'
+
+export default function App(){
+  const [peso, setPeso] = useState('');
+  const [altura, setAltura] = useState('');
+
+  const [message, setMensagem] = useState ('') 
+
+  function calcularIMC() {
+    const alt = altura / 100;
+    const imc = peso / (alt * alt);
+
+    if (imc < 18.6) {
+      setMensagem("Você está abaixo do peso! Seu IMC: " + imc.toFixed (2))
+      
+      } else if (imc>= 18.6 && imc<24.9) {
+        setMensagem("Você está no seu peso ideal! Seu IMC: " + imc.toFixed (2))
+      } else if (imc>= 24.9 && imc<34.9) {
+        setMensagem("Você está levemente acima do peso! Seu IMC: " + imc.toFixed (2))
+      } else if (imc> 34.9) {
+        setMensagem("Cuidado. Obesidade! Seu IMC: " + imc.toFixed (2))
+      } 
+  }  
+  
+  return(
+    <div className="app">
+      <h1>Calculadora IMC</h1>
+      <span>Vamos calcular seu IMC</span>
+
+      <div className="area-input">
+        <input 
+        type="text"
+        placeholder="Peso em (Kg) Ex:90"
+        value={peso}
+        onChange={ (e) => setPeso (e.target.value)}
+        />
+        <input 
+        type="text"
+        placeholder="Altura em (cm) Ex:180"
+        value={altura}
+        onChange={ (e) => setAltura (e.target.value)}
+        />
+
+        <button onClick={calcularIMC}>
+          Calcular
+        </button>
+      </div>
+
+      <h2>{message}</h2>
+    </div>
+  )
+}
