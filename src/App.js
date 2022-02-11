a3f2779249076b7824260f0f741a93180b4fabe3
Williane Delfino <willy.pinheiro.94@gmail.com>
2022-02-11 17:06:34 -0300

    Revert "set message new commit"
    This reverts commit 1dcaa03b8e126920ad5e774f5c683f2053e8ff75.

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ export default function App(){
     const imc = peso / (alt * alt);
 
     if (imc < 18.6) {
-      setMensagem("Você está no peso! Seu IMC: " + imc.toFixed (2))
+      setMensagem("Você está abaixo do peso! Seu IMC: " + imc.toFixed (2))
       
       } else if (imc>= 18.6 && imc<24.9) {
         setMensagem("Você está no seu peso ideal! Seu IMC: " + imc.toFixed (2))
